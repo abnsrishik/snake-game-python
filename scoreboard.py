83dda4ede3e4bdf16d97fd3c1e2921e5e9a45b3d
Atkuri Bala Naga Sai Narasimha Subramanya Rishik <rishikcr72401@gmail.com>
2026-05-25 15:24:30 +0530

    Update ScoreBoard class with high score functionality
    Refactor ScoreBoard class to include high score and improve score display.

--- scoreboard.py
+++ scoreboard.py
@@ -1,30 +1,34 @@
 from turtle import Turtle
-COORDINATES = (0,270)
 ALIGNMENT = "center"
-FONT = ("Arial", 15, "normal")
-
-
+FONT = ("Courier", 20, 'normal')
+SCORE_POSITION = (0,270)
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
-        self.penup()
         self.hideturtle()
-        self.color("white")
-        self.goto(COORDINATES)
-        self.update_score()
+        self.score = 0
+        self.high_score = 0
+        self.color('white')
+        self.penup()
+        self.goto(SCORE_POSITION)
+        self.update_scoreboard()
 
-
-    def update_score(self):
-        self.write(arg=f"Score = {self.score}", align=ALIGNMENT, font= FONT)
-
-    def game_over(self):
-        self.goto(0,0)
-        self.write(arg= "GAME OVER", align=ALIGNMENT, font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align = ALIGNMENT, font = FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(arg=f"Score = {self.score}", align="center", font=("Arial", 15, "normal"))
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align = ALIGNMENT, font= FONT)
 
