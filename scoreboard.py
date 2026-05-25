9efa2a008bced9504c1c04feea2d821cf09629f8
Atkuri Bala Naga Sai Narasimha Subramanya Rishik <rishikcr72401@gmail.com>
2026-05-25 15:46:40 +0530

    Implement high score persistence in ScoreBoard
    Add functionality to save and load high score from a file.

--- scoreboard.py
+++ scoreboard.py
@@ -2,20 +2,22 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 20, 'normal')
 SCORE_POSITION = (0,270)
-
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.hideturtle()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", mode = 'r') as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.penup()
         self.goto(SCORE_POSITION)
+
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
+
         self.write(f"Score: {self.score} High Score: {self.high_score}", align = ALIGNMENT, font = FONT)
 
     def increase_score(self):
@@ -25,8 +27,11 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode='w') as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
+
 
     # def game_over(self):
     #     self.goto(0, 0)
